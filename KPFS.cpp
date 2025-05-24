30e0bb6b67ebaea9043f563e0a1c95f3e39a03b4
Luiz Guilherme de Andrade Pires <luizgapires@gmail.com>
2025-05-23 22:22:17 -0300

    Add filepath.cpp e header

--- KPFS.cpp
+++ KPFS.cpp
@@ -1,5 +1,6 @@
 #include<iostream> 
-#include<fstream> 
+#include<fstream>
+#include "filepath.h"
 
 
 using namespace std;
@@ -8,65 +9,60 @@ struct Item {
     int id;
     int peso;
     int profit;
-    int solv;
+    bool solv;
 
-    Item (int _id, int p, int l , bool sol=0): id(_id), peso(p), profit(l), solv(sol) {}
+    Item (int _id, int p, int l , bool sol=false): id(_id), peso(p), profit(l), solv(sol) {}
 };
-
-
-// class item {
-//     int id;
-//     int peso;
-//     int profit;
-//     int solv;
-
-//     public:
-//         item(int id);
-
-//         void add_peso(int p);
-
-//         void add_profit(int l);
-// };
-
-// item::item(int id){
-//     this->id = id;
-// }
 
 
 int main(){
     int nI = 0; //número de itens
     int nP = 0; //número de conjuntos de penalidade
     int kS = 0; //Capacidade de mochila
+    string filepath, cenario, sc, tipo;
+    pair<string, string> par_string;
     fstream newfile;
-    
 
-    newfile.open("instances\\scenario1\\correlated_sc1\\300\\kpfs_1.txt", ios_base::in);
+
+    //Escolha do cenário
+    cout << "Escolha o cenário:" << endl;
+    cout << "1: scenario1" << endl;
+    cout << "2: scenario2" << endl;
+    cout << "3: scenario3" << endl;
+    cout << "4: scenario4" << endl;
+    cin >> cenario;
+
+    par_string = get_cenario(cenario, sc);
+    cenario = par_string.first;
+    
+    if (cenario == "erro"){
+        cerr << "Erro: Informe valores entre 1 e 4!";
+        return 1;
+    }
+    sc = par_string.second;
+   
+    cout << "Escolha o tipo do cenário:" << endl;
+    cout << "1: correlated" << endl;
+    cout << "2: fully correlated" << endl;
+    cout << "3: not correlated" << endl;
+    cin >> tipo;
+
+    tipo = get_tipo(tipo);
+    if (tipo == "erro"){
+        cerr << "Erro: Informe valores entre 1 e 3!";
+        return 1;
+    }
+    
+    filepath = "instances\\" + cenario + "\\" + tipo + sc + "\\300\\kpfs_1.txt";
+    cout << filepath << endl;
+
+    newfile.open(filepath, ios_base::in);
     
     if(newfile.is_open()){
         cout << "Arquivo aberto com sucesso." << endl;
-        if (newfile >> nI) {
-            // Leitura de nI bem-sucedida
-        } else {
-            cerr << "Erro ao ler o número de itens (nI) do arquivo." << endl;
-            newfile.close(); // Fecha o arquivo antes de sair em caso de erro parcial
-            return 1;        // Retorna um código de erro
-        }
-
-        if (newfile >> nP) {
-            // Leitura de nP bem-sucedida
-        } else {
-            cerr << "Erro ao ler o número de conjuntos de penalidade (nP) do arquivo." << endl;
-            newfile.close();
-            return 1;
-        }
-
-        if (newfile >> kS) {
-            // Leitura de kS bem-sucedida
-        } else {
-            cerr << "Erro ao ler a capacidade da mochila (kS) do arquivo." << endl;
-            newfile.close();
-            return 1;
-        }
+        newfile >> nI;
+        newfile >> nP;
+        newfile >> kS;
 
         newfile.close(); // Fecha o arquivo após terminar de usá-lo
         cout << "Arquivo fechado." << endl;
