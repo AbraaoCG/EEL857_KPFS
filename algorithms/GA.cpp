78249825867be5acb49cb598f1e876c1b8efdff3
Abraão Gomes <abraao.cg@poli.ufrj.br>
2025-06-18 13:17:45 -0300

    Ajuste na função main.cpp para escrita no caminho correto. Ajuste no GA para calculo da melhora na função objetivo.

--- algorithms/GA.cpp
+++ algorithms/GA.cpp
@@ -37,20 +37,35 @@ int calcularPeso(const Instance &inst, const vector<bool> &best_sol) {
 
 typedef pair<int, int> parValorPeso;
 
-Resultado genetic_algorithm(const Instance &inst, const fs::path &caminho, int population_size = 150, int max_generations = 1000, double crossover_rate = 0.9, double mutation_rate = 0.15, double maxGenEstagnated = 0.3, double threshold = 0.05, bool track_evolution = 0, bool verbose = 0) {
+Resultado genetic_algorithm(const Instance &inst, 
+    const fs::path &caminho, 
+    int population_size = 300, 
+    int max_generations = 1000, 
+    double crossover_rate = 0.9, 
+    double mutation_rate = 0.35, 
+    double maxGenEstagnated = 1, 
+    double threshold = 0.01, 
+    bool verbose = 1) {
+
     int n_items = inst.numItems;
     vector<vector<bool>> population(population_size, vector<bool>(n_items));
     vector<double> fitness(population_size, -1e9);
     Resultado melhorSol;
     melhorSol.valorObjetivo = -1e9;
+    
+    if(inst.numItems == 0){
+        cout << "Arquivo com problema! ";
+        return melhorSol;
+    }
 
+    // Inicializar população com soluções zeradas
     for (int i = 0; i < population_size; ++i) {
         for (int j = 0; j < n_items; ++j) {
             population[i][j] = 0;
         }
         fitness[i] = get_objective_value(population[i], inst);
     }
-
+    // Abrir arquivo para escrita de resultado.
     std::ofstream log_file(caminho);
     if (!log_file.is_open()) {
         std::cerr << "Aviso: Nao foi possivel abrir o arquivo de log para escrita: " << caminho << std::endl;
@@ -58,19 +73,20 @@ Resultado genetic_algorithm(const Instance &inst, const fs::path &caminho, int p
         log_file << "Iteracao;ValorObjetivo;Peso\n";
     }
 
+    // 2. Loop principal do algoritmo genético
     int ultimaGeracaoDeMelhora = -1;
-
     for (int generation = 0; generation < max_generations; ++generation) {
         vector<vector<bool>> new_population;
 
+        // 2.2 Reprodução
         while (new_population.size() < population_size) {
             vector<bool> parent1 = roulette_wheel_selection(population, fitness);
             vector<bool> parent2 = roulette_wheel_selection(population, fitness);
 
+            // Crossover com probabilidade definida
             if ((double)rand() / RAND_MAX < crossover_rate) {
                 int crossover_point = rand() % n_items;
                 vector<bool> child1 = parent1, child2 = parent2;
-
                 for (int i = crossover_point; i < n_items; ++i) {
                     swap(child1[i], child2[i]);
                 }
@@ -78,25 +94,24 @@ Resultado genetic_algorithm(const Instance &inst, const fs::path &caminho, int p
                 if (new_population.size() < population_size) {
                     new_population.push_back(child2);
                 }
-            } else {
+            } else { // Caso não haja, os pais são vão para a nova população
                 new_population.push_back(parent1);
                 if (new_population.size() < population_size) {
                     new_population.push_back(parent2);
                 }
             }
         }
-
+        // 2.3 Mutação
         for (auto &individual : new_population) {
             if ((double)rand() / RAND_MAX < mutation_rate) {
                 int mutation_point = rand() % n_items;
                 individual[mutation_point] = !individual[mutation_point];
             }
         }
-
+        // 2.4 Avaliação da nova população
         double generation_best_fitness = -1e9;
         vector<bool> generation_best_sol;
         int pesoTotal;
-
         for (int i = 0; i < population_size; ++i) {
             fitness[i] = get_objective_value(new_population[i], inst);
             if (fitness[i] > generation_best_fitness) {
@@ -110,7 +125,7 @@ Resultado genetic_algorithm(const Instance &inst, const fs::path &caminho, int p
         if (log_file.is_open()) {
             log_file << generation + 1 << ";" << generation_best_fitness << ";" << pesoTotal << "\n";
         }
-
+        // Armazenar melhor de todas as gerações
         if (generation_best_fitness > melhorSol.valorObjetivo) {
             int ultimaSolOtima = melhorSol.valorObjetivo;
             melhorSol.valorObjetivo = generation_best_fitness;
@@ -118,9 +133,10 @@ Resultado genetic_algorithm(const Instance &inst, const fs::path &caminho, int p
             melhorSol.pesoTotal = pesoTotal;
             double melhoraNaFuncaoObjetivo;
 
-            if (!melhorSol.valorObjetivo) {
+            if (melhorSol.valorObjetivo != 0) {
                 melhoraNaFuncaoObjetivo = (melhorSol.valorObjetivo - ultimaSolOtima) / melhorSol.valorObjetivo;
             } else {
+                // Caso a melhor solução
                 melhoraNaFuncaoObjetivo = threshold * 1.1;
             }
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
-#include "algorithms/GA.cpp"
 #include "algorithms/grasp.cpp"
 #include "algorithms/TS.cpp"
-#include "utils/interpreter.hpp"
+#include "algorithms/GA.cpp"
 #include <iostream>
+// Gera o caminho correspondente em "outputs/"
+fs::path gerarCaminhoOutput(const fs::path& inputPath, const std::string& algoritmo) {
+    fs::path relativo = fs::relative(inputPath, "instances");
+    fs::path outputPath = fs::path("outputs") / relativo;
+    outputPath.replace_filename(outputPath.stem().string() + "_" + algoritmo + ".txt");
+    return outputPath;
+}
+// Salva o resultado no arquivo correspondente
+void salvarResultado(const fs::path& caminho, const Resultado& res, const Instance& inst) {
+    fs::create_directories(caminho.parent_path());
+    std::ofstream out(caminho, std::ios_base::app);
 
-using namespace std;
-
-
+    // Adiciona um separador se o arquivo já tiver conteúdo, para melhor legibilidade
+    if (out.tellp() > 0) {
+        out << "\n----------------------------------------\n\n";
+    }
+    out << "Lucro total: " << res.lucroTotal << "\n";
+    out << "Penalidade total: " << res.penalidadeTotal << "\n";
+    out << "Valor objetivo: " << res.valorObjetivo << "\n";
+    out << "Peso na Mochila:" << res.pesoTotal << "/" << inst.capacity <<"\n";
+    out << "Tempo (ms): " << res.tempoMs << "\n";
+    out << "Itens selecionados (por índice):\n";
+    for (size_t i = 0; i < res.itensSelecionados.size(); ++i) {
+        if (res.itensSelecionados[i])
+            out << i << " ";
+    }
+    out << "\n";
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -14,29 +37,31 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::string algoritmo = "tabu";
+    std::string algoritmo = "GA";
     std::string caminho = argv[1];
     try {
         Instance inst = lerInstancia(caminho);
+        fs::path caminhoOutput = gerarCaminhoOutput(caminho, algoritmo);
+        fs::create_directories(caminhoOutput.parent_path());
+
         auto start = std::chrono::high_resolution_clock::now();
 
         Resultado res;
-        if (algoritmo == "grasp") res = grasp(inst, caminho);
+        if (algoritmo == "grasp") res = grasp(inst, caminhoOutput);
         // else if (algoritmo == "ils") res = ils(inst);
         // else if (algoritmo == "vns") res = vns(inst);
-        else if (algoritmo == "tabu") res = tabu_search(inst,caminho);
-        // else if (algoritmo == "genetic") res = genetic(inst);
+        else if (algoritmo == "tabu") res = tabu_search(inst, caminhoOutput);
+        else if(algoritmo == "GA") res = genetic_algorithm(inst, caminhoOutput);
         // else if (algoritmo == "sa") res = simulatedAnnealing(inst);
-        else if(algoritmo == "GA"){ res = genetic_algorithm(inst, caminho);
-
-        }else{
+        else {
             std::cerr << "Algoritmo desconhecido.\n";
             return 1;
         }
 
         auto end = std::chrono::high_resolution_clock::now();
         res.tempoMs = std::chrono::duration<double, std::milli>(end - start).count();
-
+        salvarResultado(caminhoOutput, res, inst);
+        
         std::cout << "Lucro: " << res.lucroTotal
                 << ", Penalidade: " << res.penalidadeTotal
                 << ", Objetivo: " << res.valorObjetivo
